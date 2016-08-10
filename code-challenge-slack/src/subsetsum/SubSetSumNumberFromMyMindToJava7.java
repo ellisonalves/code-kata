5a92761d4dac6e1b40913fc7b1a7f8d8074661b2
Ellison Alves <ellisonalves@gmail.com>
2016-08-10 10:49:35 +0200

    fixing the misunderstanding about the problem and adding support to command-line input

--- code-challenge-slack/src/subsetsum/SubSetSumNumberFromMyMindToJava7.java
+++ code-challenge-slack/src/subsetsum/SubSetSumNumberFromMyMindToJava7.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * You're given two inputs: a positive integer x and an array A of
@@ -28,55 +29,67 @@ import java.util.Set;
  */
 public class SubSetSumNumberFromMyMindToJava7 {
 
-    private static final int MAX_MAGIC_NUMBER = 29;
-    private static final int RANGE_AROUND_MAGIC_NUMBER = MAX_MAGIC_NUMBER - 5;
+    private static final int SUM = 29;
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         SubSetSumNumberFromMyMindToJava7 subSetSumNumberFromMyMindToJava7 = new SubSetSumNumberFromMyMindToJava7();
-        subSetSumNumberFromMyMindToJava7.challengeAccepted(MAX_MAGIC_NUMBER, new Integer[]{2, 8, 3, 9, 11});
-        // [11, 9, 8] = 28
+        long begin = System.nanoTime();
+        if (args.length > 0) {
+            Integer[] argsAsInteger = new Integer[args.length];
+            for (int i = 0; i < args.length; i++)
+                argsAsInteger[i] = Integer.parseInt(args[i]);
+            subSetSumNumberFromMyMindToJava7.challengeAccepted(argsAsInteger);
+        } else {
+            subSetSumNumberFromMyMindToJava7.challengeAccepted(new Integer[]{2, 8, 3, 9, 11});
+            // [11, 9, 8] = 28
 
-        subSetSumNumberFromMyMindToJava7.challengeAccepted(MAX_MAGIC_NUMBER, new Integer[]{2, 8, 3, 9, 11, 12, 13, 15, 20});
-        // [20, 8] = 28
-        // [15, 13] = 28
-        // [13, 12, 3] = 28
-        // [12, 11, 3, 2] = 28
-        // [11, 9, 8] = 28
+            subSetSumNumberFromMyMindToJava7.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, 13, 15, 20});
+            // [20, 8] = 28
+            // [15, 13] = 28
+            // [13, 12, 3] = 28
+            // [12, 11, 3, 2] = 28
+            // [11, 9, 8] = 28
 
-        subSetSumNumberFromMyMindToJava7.challengeAccepted(MAX_MAGIC_NUMBER, new Integer[]{2, 8, 3, 9, 11, 12, -1, -2, 15, 30, 38});
-        // [30, -2] = 28
-        // [15, 12, -1, -2] = 24
-        // [12, 11, 3, 2, -1, -2] = 25
-        // [11, 9, 8, -1, -2] = 25
+            subSetSumNumberFromMyMindToJava7.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, -1, -2, 15, 30, 38});
+            // [30, -2] = 28
+        }
+        long end = System.nanoTime();
+
+        System.out.println("Total time running: " + String.format("%d millis", TimeUnit.NANOSECONDS.toMillis(end - begin)));
     }
 
-    public void challengeAccepted(int magicNumber, Integer[] numbersSet) {
+    public void challengeAccepted(Integer[] numbersSet) {
         System.out.println();
         System.out.println("Output: ");
-        int[][] integers = subSetSumNumbers(magicNumber, numbersSet);
-        for (int[] array : integers) {
+        int[][] integers = subSetSumNumbers(numbersSet);
+        for (int[] array : integers)
             System.out.println(Arrays.toString(array) + " = " + sum(array));
-        }
     }
 
-    private int[][] subSetSumNumbers(final int magicNumber, final Integer[] numbersSet) {
+    private int[][] subSetSumNumbers(final Integer[] numbersSet) {
         Arrays.sort(numbersSet, Collections.reverseOrder());
-        int totalOfIterations = numbersSet.length;
         Set<int[]> finalResult = new LinkedHashSet<>();
-        for (int i = 0; i < totalOfIterations - 1; i++) {
-            int[] filter = new int[totalOfIterations];
+        int maxPossibleSum = 0;
+        for (int i = 0; i < numbersSet.length - 1; i++) {
+            int[] filter = new int[numbersSet.length];
             filter[i] = numbersSet[i];
             int index = i + 1;
-            while (index < totalOfIterations) {
+            while (index < numbersSet.length) {
                 filter[index] = numbersSet[index];
-                boolean invalidSum = !((sum(filter)) < magicNumber);
+                boolean invalidSum = !((sum(filter)) < SUM);
                 if (invalidSum)
                     filter[index] = 0;
                 index++;
             }
-            int sumResult = sum(filter);
-            if (sumResult >= RANGE_AROUND_MAGIC_NUMBER && sumResult < magicNumber) {
-                finalResult.add(removeZeros(filter));
+            int actualSetSum = sum(filter);
+            if (actualSetSum < SUM) {
+                // This logic is possible because the numbersSet is ordered (reverse order)
+                // So, the first array sum elements will always be closest from SUM.
+                if (finalResult.isEmpty()) {
+                    maxPossibleSum = actualSetSum;
+                    finalResult.add(removeZeros(filter));
+                } else if (actualSetSum == maxPossibleSum)
+                    finalResult.add(removeZeros(filter));
             }
         }
         return finalResult.toArray(new int[][]{});
@@ -102,4 +115,5 @@ public class SubSetSumNumberFromMyMindToJava7 {
                     sum += e;
         return sum;
     }
+
 }
