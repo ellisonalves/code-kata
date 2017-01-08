32a547d1df6813a0a3424c3b5e4e5b122e86b1d4
Ellison Alves <ellisonalves@gmail.com>
2017-01-08 12:40:14 +0100

    adding gradle and tests to the solution

--- codechallenges/subsets/src/main/java/subsetssum/SubsetSumProblem.java
+++ codechallenges/subsets/src/main/java/subsetssum/SubsetSumProblem.java
@@ -1,6 +1,9 @@
 package subsetssum;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,42 +27,49 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * Created by ellison on 28/07/16.
  */
-public class SubSetSumProblem {
+public class SubsetSumProblem {
 
     private static final int SUM_X = 28;
 
+    private Integer sumX;
+
+    public SubsetSumProblem(Integer sumX) {
+        this.sumX = sumX;
+    }
+
     public static void main(String... args) {
-        SubSetSumProblem subSetSumProblem = new SubSetSumProblem();
+        SubsetSumProblem subsetSumProblem = new SubsetSumProblem(SUM_X);
         if (args.length > 0) {
             Integer[] argsAsInteger = new Integer[args.length];
             for (int i = 0; i < args.length; i++)
                 argsAsInteger[i] = Integer.parseInt(args[i]);
-            subSetSumProblem.challengeAccepted(argsAsInteger);
+            subsetSumProblem.challengeAccepted(argsAsInteger);
         } else {
 
-            subSetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 6});
+            subsetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 6});
             // [11, 9, 8] = 28
 
-            subSetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, 13, 15, 20});
+            /*
+            subsetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, 13, 15, 20});
             // [20, 8] = 28
             // [15, 13] = 28
             // [13, 12, 3] = 28
             // [12, 11, 3, 2] = 28
             // [11, 9, 8] = 28
 
-            subSetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, -1, -2, 15, 30, 38});
+            subsetSumProblem.challengeAccepted(new Integer[]{2, 8, 3, 9, 11, 12, -1, -2, 15, 30, 38});
             // [30, -2] = 28
 
-            subSetSumProblem.challengeAccepted(new Integer[]
+            subsetSumProblem.challengeAccepted(new Integer[]
                     {
                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                            11, 12 ,13 ,14, 15, 16, 17, 18, 19, 20,
+                            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                             21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
                             31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
                             41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-                            51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,
+                            51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66,
                     }
-            );
+            );*/
 
             /*final int BIG_ARRAY = 1000;
             Integer[] bigSet = new Integer[BIG_ARRAY];
@@ -68,7 +78,8 @@ public class SubSetSumProblem {
                 bigSet[i] = random.nextInt(SUM_X);
             }
 
-            subSetSumProblem.challengeAccepted(bigSet);*/
+            subsetSumProblem.challengeAccepted(bigSet);
+            */
         }
     }
 
@@ -77,42 +88,39 @@ public class SubSetSumProblem {
         System.out.println("Output: ");
         long start = System.nanoTime();
 
-        int[][] integers = subSetSumNumbers(numbersSet);
+        Set<Integer[]> integers = subSetSumNumbers(numbersSet);
 
         long duration = System.nanoTime() - start;
 
-        for (int[] array : integers)
+        for (Integer[] array : integers)
             System.out.println(Arrays.toString(array) + " = " + sum(array));
 
-        System.out.println(String.format("Total of subsets: %d", integers.length));
+        System.out.println(String.format("Total of subsets: %d", integers.size()));
         System.out.println(String.format("Total time running: %d seconds", TimeUnit.SECONDS.convert(duration, TimeUnit.NANOSECONDS)));
     }
 
-    int[][] subSetSumNumbers(final Integer[] numbers) {
+    Set<Integer[]> subSetSumNumbers(final Integer[] numbers) {
         Integer[] numbersCopy = Arrays.copyOf(numbers, numbers.length);
         Arrays.sort(numbersCopy, Collections.reverseOrder());
 
-        Set<int[]> validCombinations = new LinkedHashSet<>();
+        Set<Integer[]> validCombinations = new LinkedHashSet<>();
 
-        while (numbersCopy.length > 0) {
-            int actualComparingBase = 0;
-            int[] combinations = new int[numbersCopy.length];
-            combinations[actualComparingBase] = numbersCopy[actualComparingBase];
+        for (int i = 0; i < numbersCopy.length - 1; i++) {
+            for (int j = i+1; j < numbersCopy.length; j++) {
+                Integer[] elements = Arrays.copyOfRange(numbersCopy, i, j);
+                int sum = sum(elements);
 
-            for (int j = actualComparingBase + 1; j < numbersCopy.length; j++) {
-                combinations[j] = numbersCopy[j];
-                int sum = sum(combinations);
-
-                if (sum > SUM_X)
-                    combinations[j] = 0;
-                else if (sum == SUM_X)
-                    validCombinations.add(removeZeros(Arrays.copyOf(combinations, combinations.length)));
+                if (sum == SUM_X) {
+                    validCombinations.add(elements);
+                }
             }
-
-            numbersCopy = Arrays.copyOfRange(numbersCopy, 1, numbersCopy.length);
         }
 
-        return validCombinations.toArray(new int[][]{});
+        if (validCombinations.isEmpty()) {
+            validCombinations.add(new Integer[]{});
+        }
+
+        return validCombinations;
     }
 
     private int[] removeZeros(final int[] array) {
@@ -127,7 +135,7 @@ public class SubSetSumProblem {
         return newOutputArray;
     }
 
-    private int sum(int[] elements) {
+    private int sum(Integer[] elements) {
         int sum = 0;
         if (elements != null)
             for (Integer e : elements)
---
+++ codechallenges/subsets/src/test/java/subsetssum/SubsetSumProblemTest.java
@@ -0,0 +1,39 @@
+package subsetssum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+
+/**
+ * Created by ellison on 06/01/17.
+ */
+public class SubsetSumProblemTest {
+
+    @Test
+    public void shouldRetrieveZeroSubsetsAsResult() throws Exception {
+        SubsetSumProblem subsetSumProblem = new SubsetSumProblem(1);
+
+        Integer[] emptyExpected = new Integer[]{};
+
+        Set<Integer[]> subsetReturned = subsetSumProblem.subSetSumNumbers(new Integer[]{1});
+
+        Assert.assertThat(subsetReturned, hasItem(emptyExpected));
+    }
+
+    @Test
+    public void shouldRetrieveOneSubsetAsResult() throws Exception {
+        final Integer SUM_X = 28;
+
+        Integer[] subsetExpected = new Integer[]{11, 9, 8};
+
+        SubsetSumProblem subsetSumProblem = new SubsetSumProblem(SUM_X);
+
+        Set<Integer[]> subsetReturned = subsetSumProblem.subSetSumNumbers((new Integer[]{2, 8, 3, 9, 11}));
+
+        Assert.assertThat(subsetReturned, hasItem(subsetExpected));
+    }
+
+}
